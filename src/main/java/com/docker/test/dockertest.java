547d1025fbbd59b3db8100839b96310cd62ef543
Devesh <deveshghate2000@gmail.com>
2025-08-20 16:37:15 +0530

    controller Added

---
+++ src/main/java/com/docker/test/dockertest.java
@@ -0,0 +1,13 @@
+package com.docker.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestControllerg
+
+public class dockertest {
+    @GetMapping("/message")
+    public String testingcode(){
+        return"Hello from ci/cd";
+    }
+}
