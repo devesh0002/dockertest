2e732ced1c97fbcd32ce53ab27d6f811830d8b95
Devesh <deveshghate2000@gmail.com>
2025-08-21 12:35:31 +0530

    controller updated

--- src/main/java/com/docker/test/dockertest.java
+++ src/main/java/com/docker/test/dockertest.java
@@ -3,7 +3,7 @@ package com.docker.test;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestControllerg
+@RestController
 
 public class dockertest {
     @GetMapping("/message")
